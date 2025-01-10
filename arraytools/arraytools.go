d0c3333d76f9009c1488a990e723dcb39e5ccc61
BananenGelb <Benedict.lotter@gmx.de>
2025-01-10 15:55:54 +0100

    10-01-2025

--- arraytools/arraytools.go
+++ arraytools/arraytools.go
@@ -4,10 +4,17 @@ package arraytools
 // Multipliziert jedes Element des Arrays mit dem Faktor.
 func ScalarMult(a []float64, factor float64) {
 	// TODO
+	for i, wert := range a { //zeihen den wert aus der Postition "pos"
+		a[i] = wert * factor //multiplizieren mit dem wert der position und setzt es an Position wieder ein
+	}
+
 }
 
 // Add erwartet zwei Arrays der gleichen Länge.
 // Addiert die Elemente der Arrays paarweise.
 func Add(a, b []float64) {
 	// TODO
+	for i := range a {
+		a[i] += b[i]
+	}
 }
--- matrixtools/matrixtools.go
+++ matrixtools/matrixtools.go
@@ -1,15 +1,22 @@
 package matrixtools
 
+import "matrices/arraytools"
+
 // GetRow liefert die i-te Zeile der Matrix m.
 func GetRow(m [][]float64, i int) []float64 {
 	// TODO
-	return []float64{}
+	result := m[i]
+
+	return result
 }
 
 // GetCol liefert die j-te Spalte der Matrix m.
 func GetCol(m [][]float64, j int) []float64 {
 	col := make([]float64, len(m))
 	// TODO
+	for i := range m {
+		col[i] = m[i][j]
+	}
 	return col
 }
 
@@ -17,18 +24,24 @@ func GetCol(m [][]float64, j int) []float64 {
 // Addiert die beiden Zeilen paarweise und speichert das Ergebnis in der ersten Zeile.
 func AddRows(m [][]float64, i, j int) {
 	// TODO
+	arraytools.Add(m[i], m[j]) //arraytool eingefügt und dadurch enzelne spalten der Matritzen aufaddieren
+
 }
 
 // ScalarMultRow erwartet eine Matrix, eine Zeilennummer und einen skalaren Faktor.
 // Multipliziert die Zeile mit dem Faktor und speichert das Ergebnis in der Zeile.
 func ScalarMultRow(m [][]float64, i int, factor float64) {
 	// TODO
+	arraytools.ScalarMult(m[i], factor)
 }
 
 // Transpose erwartet eine Matrix und liefert ihre Transponierte.
 // D.h. alle Zeilen der ersten Matrix werden zu Spalten der Transponierten und umgekehrt.
 func Transposed(m [][]float64) [][]float64 {
 	transposed := make([][]float64, len(m[0]))
-	// TODO
+	// TODO //Transposed = m[0]
+	for i := range m[0] { //m fragt ab wie viele zeilen hast du und [0] fragt ab, wie viele Spalten wir haben
+		transposed[i] = GetCol(m, i)
+	}
 	return transposed
 }
